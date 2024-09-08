04d4740b38e651bf61248f077124f800e92b437b
Chase Carlson <chasecarlson08@gmail.com>
2024-09-08 16:16:13 -0700

    Strip code so we do it again

--- bst/test.h
+++
@@ -1,6 +0,0 @@
-#ifndef DSA_BST_TEST_H
-#define DSA_BST_TEST_H
-
-void bst_test_run_all();
-
-#endif
--- heap/test.h
+++
@@ -1,4 +0,0 @@
-#ifndef DS_HEAP_TEST_H
-#define DS_HEAP_TEST_H
-void heap_run_all_tests();
-#endif
